bc025a06eacee2bfae8ef12be2eaf2ff310e81e8
David Delassus <david.jose.delassus@gmail.com>
2022-03-27 01:42:45 +0100

    :sparkles: add command data templating (closes #8)

--- src/cli/exec.rs
+++ src/cli/exec.rs
@@ -2,14 +2,22 @@ use crate::{Result, Host};
 
 use clap::ArgMatches;
 use serde_json::{json, Value};
+use tinytemplate::{TinyTemplate, format_unescaped};
 
 pub fn run(hosts: Vec<Host>, matches: &ArgMatches) -> Result<()> {
-  let cmd = get_command(matches.values_of("cmd"));
+  let cmd_tmpl = get_command(matches.values_of("cmd"));
 
   let results: Vec<Value> = hosts
     .iter()
     .map(|host| {
+      let cmd = render_command(host, cmd_tmpl.clone())?;
+      Ok((host, cmd))
+    })
+    .collect::<Result<Vec<(&Host, String)>>>()?
+    .into_iter()
+    .map(|(host, cmd)| {
       eprintln!("Executing command on {}...", host.id);
+
       host.exec(&cmd)
         .map_or_else(
           |err| {
@@ -44,4 +52,14 @@ fn get_command(arg: Option<clap::Values<'_>>) -> String {
     .map(|vals| vals.collect::<Vec<_>>())
     .map(|argv| shell_words::join(argv))
     .unwrap()
+}
+
+fn render_command(host: &Host, cmd_tmpl: String) -> Result<String> {
+  let mut tt = TinyTemplate::new();
+  tt.set_default_formatter(&format_unescaped);
+  tt.add_template("cmd", cmd_tmpl.as_str())?;
+
+  let ctx = json!({ "host": host });
+  let cmd = tt.render("cmd", &ctx)?;
+  Ok(cmd)
 }
