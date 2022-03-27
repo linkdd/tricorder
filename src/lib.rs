c5147dadd76760ce3faad9b7dd77c0f33a24d959
David Delassus <david.jose.delassus@gmail.com>
2022-03-27 01:47:42 +0100

    :fire: remove useless extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-extern crate toml;
-extern crate serde_json;
-extern crate serde_derive;
-
-extern crate clap;
-extern crate ssh2;
-extern crate is_executable;
-extern crate shell_words;
-
 pub mod inventory;
 pub mod host;
 pub mod cli;
