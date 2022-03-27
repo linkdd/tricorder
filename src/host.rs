9cec5254537139f1198d24f1db32f09757740f7e
David Delassus <david.jose.delassus@gmail.com>
2022-03-28 00:27:37 +0200

    :sparkles: add send_* functions to Host

--- src/host.rs
+++ src/host.rs
@@ -6,8 +6,14 @@ use ssh2::Session;
 
 use std::{
   collections::HashMap,
-  io::prelude::*,
+  io::{
+    prelude::*,
+    BufRead,
+    BufReader,
+  },
   net::TcpStream,
+  fs::File,
+  path::Path,
 };
 
 
@@ -25,12 +31,7 @@ pub struct Host {
 
 impl Host {
   pub fn exec(&self, command: &str) -> Result<(i32, String)> {
-    let sock = TcpStream::connect(self.address.clone())?;
-    let mut sess = Session::new()?;
-    sess.set_tcp_stream(sock);
-    sess.handshake()?;
-    sess.userauth_agent(&self.user)?;
-
+    let sess = self.get_session()?;
     let mut channel = sess.channel_session()?;
     channel.exec(command)?;
 
@@ -41,6 +42,80 @@ impl Host {
     let exit_code = channel.exit_status()?;
 
     Ok((exit_code, output))
+  }
+
+  pub fn send_data(
+    &self,
+    remote_path: String,
+    file_mode: i32,
+    file_size: u64,
+    data: String,
+  ) -> Result<()> {
+    let sess = self.get_session()?;
+    let mut channel = sess.scp_send(
+      Path::new(&remote_path),
+      file_mode,
+      file_size,
+      None
+    )?;
+
+    channel.write(data.as_bytes())?;
+    channel.send_eof()?;
+    channel.wait_eof()?;
+    channel.close()?;
+    channel.wait_close()?;
+
+    Ok(())
+  }
+
+  pub fn send_file(
+    &self,
+    local_path: String,
+    remote_path: String,
+    file_mode: i32,
+    file_size: u64,
+  ) -> Result<()> {
+    let sess = self.get_session()?;
+    let mut channel = sess.scp_send(
+      Path::new(&remote_path),
+      file_mode,
+      file_size,
+      None
+    )?;
+
+    let file = File::open(&local_path)?;
+    let block_size = 4 * 1024 * 1024; // 4 megabytes
+    let mut reader = BufReader::with_capacity(block_size, file);
+
+    loop {
+      let buffer = reader.fill_buf()?;
+      let length = buffer.len();
+
+      if length > 0 {
+        channel.write(buffer)?;
+      }
+      else {
+        channel.send_eof()?;
+        channel.wait_eof()?;
+        channel.close()?;
+        channel.wait_close()?;
+        break;
+      }
+
+      reader.consume(length);
+    }
+
+    Ok(())
+  }
+
+  fn get_session(&self) -> Result<Session> {
+    let sock = TcpStream::connect(self.address.clone())?;
+    let mut sess = Session::new()?;
+    sess.set_tcp_stream(sock);
+    sess.handshake()?;
+    sess.userauth_agent(&self.user)?;
+
+    Ok(sess)
   }
 
   pub fn default_user() -> String {
