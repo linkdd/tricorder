cabf1bbd54491f3bebae5a633afaf878f2402760
David Delassus <david.jose.delassus@gmail.com>
2022-04-02 09:15:13 +0200

    :white_check_mark: finalize exec test suite

--- tests/all/tasks/common.rs
+++ tests/all/tasks/common.rs
@@ -1,12 +1,20 @@
 use tricorder::prelude::*;
-use std::env;
+use std::{panic, env};
 
-pub fn setup_context() -> Result<Inventory> {
-  let cwd = env::current_dir()?;
+pub fn within_context<T>(test_fn: T) -> ()
+  where T: FnOnce(Inventory) -> () + panic::UnwindSafe
+{
+  let cwd = env::current_dir().unwrap();
   let test_dir = cwd.join("tests").join("tricorder");
-  env::set_current_dir(test_dir)?;
+  env::set_current_dir(test_dir).unwrap();
 
-  let inventory = Inventory::from_file("./inventory.toml")?;
+  let inventory = Inventory::from_file("./inventory.toml").unwrap();
 
-  Ok(inventory)
+  let result = panic::catch_unwind(|| {
+    test_fn(inventory)
+  });
+
+  env::set_current_dir(cwd).unwrap();
+
+  assert!(result.is_ok());
 }
